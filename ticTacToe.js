4446bd0b4715047e53b89d9853a4a0982e9cf45a
venelinkolev <venelinkolev@gmail.com>
2022-12-15 13:38:48 +0200

    Tic Tac Toe

--- ticTacToe.js
+++ ticTacToe.js
@@ -6,48 +6,118 @@ function ticTacToe(arr) {
   let secondTurn = false;
 
   let dashboard = [
-    [false, false, false],
-    [false, false, false],
-    [false, false, false],
+    ['false', 'false', 'false'],
+    ['false', 'false', 'false'],
+    ['false', 'false', 'false'],
   ];
+  const gameWin = (dashboard) => {
+    const length = dashboard.length;
+    let isWin = false;
+
+    if (
+      dashboard[0][0] === dashboard[0][1] &&
+      dashboard[0][0] === dashboard[0][2] &&
+      dashboard[0][0] !== 'false'
+    )
+      isWin = true;
+    else if (
+      dashboard[0][0] === dashboard[1][0] &&
+      dashboard[0][0] === dashboard[2][0] &&
+      dashboard[0][0] !== 'false'
+    )
+      isWin = true;
+    else if (
+      dashboard[1][0] === dashboard[1][1] &&
+      dashboard[1][0] === dashboard[1][2] &&
+      dashboard[1][0] !== 'false'
+    )
+      isWin = true;
+    else if (
+      dashboard[2][0] === dashboard[2][1] &&
+      dashboard[2][0] === dashboard[2][2] &&
+      dashboard[2][0] !== 'false'
+    )
+      isWin = true;
+    else if (
+      dashboard[0][1] === dashboard[1][1] &&
+      dashboard[0][1] === dashboard[2][1] &&
+      dashboard[0][1] !== 'false'
+    )
+      isWin = true;
+    else if (
+      dashboard[0][2] === dashboard[1][2] &&
+      dashboard[0][2] === dashboard[2][2] &&
+      dashboard[0][2] !== 'false'
+    )
+      isWin = true;
+    else if (
+      dashboard[0][0] === dashboard[1][1] &&
+      dashboard[0][0] === dashboard[2][2] &&
+      dashboard[0][0] !== 'false'
+    )
+      isWin = true;
+    else if (
+      dashboard[2][0] === dashboard[1][1] &&
+      dashboard[2][0] === dashboard[0][2] &&
+      dashboard[2][0] !== 'false'
+    )
+      isWin = true;
+
+    return isWin;
+  };
 
   const gameEnd = (dashboard) => {
     let length = dashboard.length;
     let isEnd = true;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length; j++) {
-        if (dashboard[i][j] === false) isEnd = false;
+        if (dashboard[i][j] === 'false') isEnd = false;
       }
     }
     return isEnd;
   };
 
   const alreadyTakenPlace = (row, column, dashboard) => {
-    if (dashboard[row][column] !== false) return true;
+    if (dashboard[row][column] !== 'false') return true;
+    else return false;
+  };
+
+  const printMatrix = (dashboard) => {
+    for (let i of dashboard) {
+      let buffer = '';
+      for (let j of i) {
+        buffer += j + '\t';
+      }
+      console.log(buffer);
+    }
   };
 
   while (arr[0] !== undefined) {
-    let [row, column] = arr.shift().split(' ');
-
-    if (firstPlayer) {
+    let [row, column] = arr.shift().split(' ').map(Number);
+    if (firstTurn) {
       if (gameEnd(dashboard)) {
         console.log('The game ended! Nobody wins :(');
+        printMatrix(dashboard);
         break;
       }
-
       if (alreadyTakenPlace(row, column, dashboard)) {
         console.log('This place is already taken. Please choose another!');
         continue;
       } else {
-        dashboard[(row, column)] = firstPlayer;
+        dashboard[row][column] = firstPlayer;
         firstTurn = false;
         secondTurn = true;
       }
-    }
 
-    if (secondPlayer) {
+      if (gameWin(dashboard)) {
+        console.log(`Player ${firstPlayer} wins!`);
+        printMatrix(dashboard);
+        break;
+      }
+    } else if (secondTurn) {
       if (gameEnd(dashboard)) {
         console.log('The game ended! Nobody wins :(');
+        printMatrix(dashboard);
         break;
       }
 
@@ -55,14 +125,18 @@ function ticTacToe(arr) {
         console.log('This place is already taken. Please choose another!');
         continue;
       } else {
-        dashboard[(row, column)] = secondPlayer;
+        dashboard[row][column] = secondPlayer;
         firstTurn = true;
         secondTurn = false;
       }
+
+      if (gameWin(dashboard)) {
+        console.log(`Player ${secondPlayer} wins!`);
+        printMatrix(dashboard);
+        break;
+      }
     }
   }
-
-  console.log();
 }
 
 ticTacToe([
@@ -86,6 +160,8 @@ ticTacToe([
 
   '0 0',
 ]);
+
+console.log('-----------------');
 ticTacToe([
   '0 0',
   '0 0',
@@ -98,6 +174,8 @@ ticTacToe([
   '2 2',
   '2 1',
 ]);
+
+console.log('-----------------');
 ticTacToe([
   '0 1',
 
