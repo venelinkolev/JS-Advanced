4d5b3f762a7fd80a9a870dd8c66a32d6fdad26d3
venelinkolev <venelinkolev@gmail.com>
2023-01-03 17:35:37 +0200

    DOM

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,19 +1,25 @@
 function solve() {
   let text = document.getElementById('input').value;
+  document.getElementById('output').innerHTML = '';
   let finalArray = [];
   let resultArray = text.split('.').filter((el) => el.length !== 0);
+  debugger;
   let buffer = '';
 
   for (let i = 1; i <= resultArray.length; i++) {
-    finalArray.push(resultArray[i - 1]);
-    //debugger;
+    finalArray.push(resultArray[i - 1].trim());
+    debugger;
     if (i % 3 === 0) {
-      buffer += `<p>${finalArray.join('. ')}.</p>` + '';
+      buffer += `<p>${finalArray.join('. ')}.</p>`;
       finalArray = [];
     }
     if (i === resultArray.length) {
-      buffer += `<p>${finalArray.join('. ')}.</p>`;
-      finalArray = [];
+      if (resultArray.length === 1) {
+        buffer = `<p>${finalArray[0]}.</p>`;
+      } else if (finalArray.length !== 0) {
+        buffer += `<p>${finalArray.join('. ')}.</p>`;
+        finalArray = [];
+      }
     }
   }
 
--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,20 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+  //   ["PizzaHut - Peter
+  // 500, George 300, Mark
+  // 800",
+  // "TheLake - Bob 1300,
+  // Joe 780, Jane 660"]
+
+  function onClick() {
+    let input = JSON.parse(document.getElementById('inputs').children[1].value);
+    console.log(input);
+    console.log(typeof input);
+    while (input[0] !== undefined) {
+      let currentRestaurant = input.shift();
+
+      console.log(currentRestaurant);
+    }
+  }
 }
