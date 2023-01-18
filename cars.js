af300f7007ced6501ad20617dc3c5dc1cc6901e6
Venelin Kolev <venelinkolev@gmail.com>
2023-01-19 00:41:04 +0200

    Cars

--- cars.js
+++ cars.js
@@ -3,19 +3,23 @@ function cars(arr) {
 
   const closure = {
     create: (name, inherits, name2) => {
-      //     (object[name] = inherits ? Object.create(object[name2]) : {}),
+      //(object[name] = inherits ? Object.create(object[name2]) : {}),
+
       if (inherits === undefined) {
         object[name] = {};
       } else {
-        Object.create(object[name2]);
+        //debugger;
+        object[name] = Object.create(object[name2]);
+        //debugger;
+        //object[name] = newObj;
       }
-    },
-    // console.log(nameOne, inherit, nameTwo);
 
+      // console.log(nameOne, inherit, nameTwo);
+    },
     set: (name, key, value) => {
       object[name][key] = value;
     },
-    print: (name) => {},
+    print: (name) => { },
   };
 
   arr.forEach((element) => {
@@ -27,11 +31,34 @@ function cars(arr) {
   console.log(object);
 }
 
+/*
+function cars(input) {
+
+  const data = {};
+
+  const result = {
+      create: (name, inherits, name2) => (data[name] = inherits ? Object.create(data[name2]) : {}),
+      set: (name, key, value) => (data[name][key] = value),
+      print: (name) => {
+          const entry = [];
+          for (const key in data[name]) {
+              entry.push(`${key}:${data[name][key]}`);
+          }
+          console.log(entry.join(","));
+      },
+  };
+debugger;
+  input.forEach((line) => {
+      const [command, name, key, value] = line.split(" ");
+      result[command](name, key, value);
+  });
+}
+*/
 cars([
   'create c1',
   'create c2 inherit c1',
   'set c1 color red',
-  // 'set c2 model new',
-  // 'print c1',
-  // 'print c2',
+  'set c2 model new',
+  'print c1',
+  'print c2',
 ]);
