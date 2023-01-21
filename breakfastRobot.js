17b8cf5c6a528b1b75a1e96e9e02f8b5e5488aa1
venelinkolev <venelinkolev@gmail.com>
2023-01-21 12:58:06 +0200

    Robot

--- breakfastRobot.js
+++ breakfastRobot.js
@@ -6,59 +6,60 @@ burger - made with 5 carbohydrate, 7 fat and 3 flavour
 eggs - made with 5 protein, 1 fat and 1 flavour
 turkey - made with 10 protein, 10 carbohydrate, 10 fat and 10 flavour
 */
-  class Recipe {
-    constructor(nameProduct, protein, carbohydrate, fat, flavours) {
-      this.nameProduct = nameProduct;
-      this.protein = protein;
-      this.carbohydrate = carbohydrate;
-      this.fat = fat;
-      this.flavours = flavours;
-    }
-  }
+  // class Recipe {
+  //   constructor(nameProduct, protein, carbohydrate, fat, flavours) {
+  //     this.nameProduct = nameProduct;
+  //     this.protein = protein;
+  //     this.carbohydrate = carbohydrate;
+  //     this.fat = fat;
+  //     this.flavours = flavours;
+  //   }
+  // }
 
-  const apple = new Recipe('apple', 0, 1, 0, 2);
-  const lemonade = new Recipe('lemonade', 0, 10, 0, 20);
-  const burger = new Recipe('burger', 0, 5, 7, 3);
-  const eggs = new Recipe('eggs', 5, 0, 1, 1);
-  const turkey = new Recipe('turkey', 10, 10, 10, 10);
+  // const apple = new Recipe('apple', _, 1, _, 2);
+  // const lemonade = new Recipe('lemonade', _, 10, _, 20);
+  // const burger = new Recipe('burger', 0, 5, 7, 3);
+  // const eggs = new Recipe('eggs', 5, 0, 1, 1);
+  // const turkey = new Recipe('turkey', 10, 10, 10, 10);
 
-  // const apple = {
-  //   carbohydrate: 1,
-  //   flavours: 2,
-  // };
-  // const lemonade = {
-  //   carbohydrate: 10,
+  // console.log(apple);
 
-  //   flavours: 20,
-  // };
-  // const burger = {
-  //   carbohydrate: 5,
-  //   fat: 7,
-  //   flavours: 3,
-  // };
-  // const eggs = {
-  //   protein: 5,
-
-  //   fat: 1,
-  //   flavours: 1,
-  // };
-  // const turkey = {
-  //   protein: 10,
-  //   carbohydrate: 10,
-  //   fat: 10,
-  //   flavours: 10,
-  // };
-
-  const recipes = [];
-  recipes.push(apple, lemonade, burger, eggs, turkey);
+  const recipes = {
+    apple: {
+      carbohydrate: 1,
+      flavour: 2,
+    },
+    lemonade: {
+      carbohydrate: 10,
+      flavour: 20,
+    },
+    burger: {
+      carbohydrate: 5,
+      fat: 7,
+      flavour: 3,
+    },
+    eggs: {
+      protein: 5,
+      fat: 1,
+      flavour: 1,
+    },
+    turkey: {
+      protein: 10,
+      carbohydrate: 10,
+      fat: 10,
+      flavour: 10,
+    },
+  };
 
   console.log(recipes);
+  // recipes.push(apple, lemonade, burger, eggs, turkey);
+
   //console.log(turkey);
   const stored = {
     protein: 0,
     carbohydrate: 0,
     fat: 0,
-    flavours: 0,
+    flavour: 0,
   };
 
   /*
@@ -69,13 +70,48 @@ report - returns information about the stored microelements, in the order descri
 
   const command = {
     restock: (microelement, quantity) => {
-      stored[microelement] += quantity;
+      stored[microelement] += Number(quantity);
       return 'Success';
     },
 
     prepare: (recipe, quantity) => {
-      let currentRecipe = recipes.find((el) => el.nameProduct === recipe);
-      return currentRecipe;
+      let currentRecipe = Object.entries(recipes).find(
+        (el) => el[0] === recipe
+      );
+      //let currentRecipeQuantity = Object.entries(currentRecipe[1]);
+
+      // currentRecipeQuantity.forEach((line) => {
+      //   line[1] = Number(line[1]) * quantity;
+      // });
+      for (let key in currentRecipe[1]) {
+        let microelement = currentRecipe[1][key];
+        microelement *= quantity;
+      }
+
+      let currentMicroelementValue = 0;
+      let microelementValue = 0;
+      let currentMicroelement = '';
+      let isEnough = true;
+      for (let key in currentRecipe[1]) {
+        currentMicroelementValue = stored[key];
+        microelementValue = currentRecipe[1][key];
+
+        if (currentMicroelementValue - microelementValue < 0) {
+          isEnough = false;
+          currentMicroelement = key;
+          break;
+        }
+      }
+
+      if (!isEnough) {
+        return `Error: not enough ${currentMicroelement} in stock`;
+      } else {
+        for (let key in stored) {
+          stored[key] -= currentRecipe[1][key];
+        }
+        return 'Success';
+      }
+      //return currentRecipe[1];
     },
 
     report: () => {},
@@ -83,6 +119,7 @@ report - returns information about the stored microelements, in the order descri
 
   return (manager) => {
     let [currentCommand, element, quantity] = manager.split(' ');
+
     return command[currentCommand](element, quantity);
   };
   //console.log(apple.carbohydrate);
